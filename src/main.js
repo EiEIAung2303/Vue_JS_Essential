d083cdcc2115c16a882ea83ed83586bf20fa41a3
eieiaung2303 <eieiaung2303@gmail.com>
2021-01-02 12:03:28 +0630

    Using Variables and Types

--- src/main.js
+++ src/main.js
@@ -1,8 +1,29 @@
-import Vue from 'vue'
-import App from './App.vue'
+//using let keyword
+//using variables and types
+function messageFunction(name,weather) {
+  let message = "Hello, Admin";
+  if (weather == "sunny") {
+    let message = "It is a nice day";
+    console.log(message);
+  } else {
+    let message = "It is " + weather + "today";
+    console.log(message);
+  }
+  console.log(message);
+}
 
-Vue.config.productionTip = false
+messageFunction("Admin", "snow ");
 
-new Vue({
-  render: h => h(App),
-}).$mount('#app')
+// function messageFunction(name,weather) {
+//   var message = "Hello, Admin";
+//   if (weather == "sunny") {
+//    var message = "It is a nice day";
+//     console.log(message);
+//   } else {
+//     var message = "It is " + weather + "today";
+//     console.log(message);
+//   }
+//   console.log(message);
+// }
+
+// messageFunction("Admin", "sunny ");
