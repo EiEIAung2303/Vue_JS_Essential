4cfa47a6e2f2384ff70f0e4a61c29d4007d97a6a
eieiaung2303 <eieiaung2303@gmail.com>
2021-01-02 13:34:49 +0630

    Using Template Strings   sign

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
-import Vue from 'vue'
-import App from './App.vue'
-
-Vue.config.productionTip = false
-
-new Vue({
-  render: h => h(App),
-}).$mount('#app')
+//10.Using Template Strings
+//let message = "It is" + weather + "today";
+//same as below
+function messageFunction(weather) {
+  let message = `It is ${weather} today`;
+  console.log(message);
+}
+messageFunction("raining");
