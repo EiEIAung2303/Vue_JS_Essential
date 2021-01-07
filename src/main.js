f67fdb54d7dc9f43cc6cbc8690a110b60afaf568
eieiaung2303 <eieiaung2303@gmail.com>
2021-01-07 22:19:03 +0630

    Array

--- src/main.js
+++ src/main.js
@@ -1,8 +1,34 @@
-import Vue from 'vue'
-import App from './App.vue'
+//Working With Arrays
+// let myArray = new Array();
+// myArray[0] = 100;
+// myArray[1] = "Adam";
+// myArray[2] = true;
+// console.log(`Index 0 : ${myArray[1]}`);
 
-Vue.config.productionTip = false
+// let myArray = [100, "Adam", false];
+// myArray[1]  = "John Adam";
+// console.log(`Index 0 : ${myArray[1]}`);
 
-new Vue({
-  render: h => h(App),
-}).$mount('#app')
+//For Each Loop
+let users = ["maung maung", "aung aung", "kaung kaung"];
+// console.log(users[0]);
+// console.log(users[1]);
+// console.log(users[2]);
+
+// for (let i =0; i < users.length; i++) {
+//     console.log(`For Loop ${i} : ${users[i]}`);
+// }
+
+// users.forEach((user) => {
+//     console.log(`For Each loop`, user);
+// });
+
+//Spread Operator
+console.log(users[0], users[1], users[2]);
+console.log(...users);
+console.log(...users, "saung saung");
+
+
+
+
+
