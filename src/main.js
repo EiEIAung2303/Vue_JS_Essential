f7edf7941eec0200ec30d0306a1591928691c981
eieiaung2303 <eieiaung2303@gmail.com>
2021-01-18 17:56:40 +0630

    modules and promises

--- src/main.js
+++ src/main.js
@@ -1,18 +1,35 @@
-import additionFunction from "./math/sum";
+//import additionFunction from "./math/sum";
 //import {multiply,subtract,divide} from "./math/operation";
 //import {multiply,subtract,divide as division} from "./math/operation";
 //as object
-// import * as otp from "./math/operation";
+//import * as otp from "./math/operation";
+
 
 import * as otp from "./math";
 
 let data = [100, 200, 300, 400, 500];
-console.log(additionFunction(data));
+//console.log(additionFunction(data));
 // console.log(multiply(data));
 // console.log(subtract(1500,data));
 // console.log(division(300,100));
-console.log(otp.multiply(data));
-console.log(otp.subtract(500, data));
-console.log(otp.divide(500,100));
-console.log(otp.means(data));
+// console.log(otp.multiply(data));
+// console.log(otp.subtract(500, data));
+// console.log(otp.divide(500,100));
+// console.log(otp.means(data));
 
+console.log(`Addition`, otp.additionFunction(data));
+
+//console.log(`Asyncous Call`, otp.asyncAdd(data));
+
+
+otp.asyncAdd(data).then((result)=>console.log(result));
+
+
+
+//async await function
+async function doTask() {
+  let total = await otp.asyncAdd(data);
+  console.log(total);
+}
+
+doTask();
--- src/math/index.js
+++ src/math/index.js
@@ -4,6 +4,21 @@ export function means(values) {
     return additionFunction(values)/values.length;
 }
 
-export {additionFunction}
+
+
+
+//get data from network
+export function asyncAdd(values) {
+    return new Promise((resolve) => {
+        setTimeout(()=>{
+            let total = additionFunction(values);
+            console.log(`Async Total : ${total}`);
+            resolve(total);
+          },2000);
+    });
+    
+  }
+
+  export {additionFunction}
 
 export * from "./operation";
